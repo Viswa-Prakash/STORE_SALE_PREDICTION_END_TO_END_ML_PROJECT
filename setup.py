262a35bf0636011942b182f6f8d8a5658c07effb
Viswa-Prakash <prakashviswa1990@gmail.com>
2023-09-20 22:16:43 +0530

    sales_prediction_completed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Sales Prediction",
+    version="0.0.1",
+    description="Machine Learning Project",
+    author="Viswa_Prakash",
+    packages=find_packages(),
+    license="MIT"
+)
